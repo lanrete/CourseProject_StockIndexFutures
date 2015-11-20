0bdd1316a7743fce52fbf3341f00b9788f4af080
lanrete <jeorito@gmail.com>
2015-11-20 23:52:55 +0800

    Source Code

---
+++ CourseProject.r
@@ -0,0 +1,39 @@
+options(scipen=999)
+
+sha000016 <- read.csv("F:/HW1/000016.csv")
+sha000016$date = as.POSIXct(sha000016$date)
+sha000016 <- subset.data.frame(sha000016, 
+                               (date >= as.POSIXct("2015-01-01") & 
+                                (date <= as.POSIXct("2015-09-30"))),
+                               select = c(date,Code,Close,PreClose))
+sha000016$Rate = (sha000016$Close-sha000016$PreClose) / sha000016$PreClose
+
+trans <- function(x) {
+  return(paste0("F:/HW1/",x,".csv"))
+}
+
+list <- c(600000,600010,600015,600016,600018,600028,600030,600036,600048,600050,
+          600089,600104,600109,600111,600150,600256,600406,600518,600519,600583,
+          600585,600637,600690,600837,600887,600893,600958,600999,601006,601088,
+          601166,601169,601186,601288,601318,601328,601390,601398,601601,601628,
+          601668,601688,601766,601800,601818,601857,601901,601988,601989,601998)
+beta <- NULL
+risk <- NULL
+
+for (i in 1:50){
+    temp <- read.csv(trans(list[i]))
+    temp$date = as.POSIXct(temp$date)
+    temp <- subset.data.frame(temp, 
+                              (date >= as.POSIXct("2015-01-01") &
+                               (date <= as.POSIXct("2015-09-30"))),
+                              select = c(date,Code,Close,PreClose))
+    temp$Rate <- (temp$Close-temp$PreClose) / temp$PreClose 
+    cal <- merge(sha000016,temp,by="date")
+    fit <- summary(lm(cal$Rate.y~cal$Rate.x))
+    beta <- c(beta,fit$coef[2,1])
+    risk <- c(risk,fit$r.squared)
+}
+
+result <- cbind(beta,risk)
+row.names(result) <- as.character(list)
+result <- round(result, digits = 5)
